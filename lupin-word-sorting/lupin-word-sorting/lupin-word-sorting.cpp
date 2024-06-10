a9124012f3f3b170e11cf2ae6b37f20b973c3ddb
nicolas-combe <150806476+nicolas-combe@users.noreply.github.com>
2024-06-10 14:03:28 +0200

    create start solution containing project and gtests executables

---
+++ lupin-word-sorting/lupin-word-sorting/lupin-word-sorting.cpp
@@ -0,0 +1,9 @@
+// lupin-word-sorting.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+}
+
---
+++ lupin-word-sorting/tests/gtest.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(Test, Test)
+{
+	EXPECT_TRUE(true);
+}
